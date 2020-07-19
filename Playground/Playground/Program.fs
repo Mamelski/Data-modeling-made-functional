fdfb12c22424999a3a0e04be5e00fc8d574b5e7c
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-07-19 17:48:24 +0200

    Some fun with types

--- Playground/Playground/Program.fs
+++ Playground/Playground/Program.fs
@@ -1,16 +1,50 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.ComponentModel.DataAnnotations
+open System.ComponentModel.DataAnnotations
+open System.Reflection.Metadata.Ecma335
+open Microsoft.VisualBasic
 
+// Functions
 let add1 x = + 1
 let add2 x y = x + y
 
 let squarePlusOne x =
     let square = x * x
-    square + 1
-    
+    square + 1    
 let equal x y=
     x = y
+    
+// Compositions
+type AppleVariety =
+ | GoldenDelicious
+ | GrannySmith
+ | Fuji
+ 
+type BanaVariety =
+ | Cavendish
+ | GrosMichel
+ | Manzano
+ 
+type CherryVariety =
+ | Montmorency
+ | Bing
+ 
+type FruitSalad = {
+     Apple: AppleVariety
+     Banana: BanaVariety
+     Cherries: CherryVariety
+ }
+ 
+type FruitSnack =
+    | Apple of AppleVariety
+    | Banana of BanaVariety
+    | Cherries of CherryVariety
+
+// Same as type ProductCode = ProductCode of string
+type ProductCode =
+    | ProductCode of string
 
 [<EntryPoint>]
 let main argv =
