9f700997d378f83c94fa45a7e75d2a10d68e0c98
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-07-19 16:38:10 +0200

    First tests with functions

---
+++ Playground/Playground/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let add1 x = + 1
+let add2 x y = x + y
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    
+    let add1res = add1 10
+    let add2res = add2 10 10
+    
+    printf "%d %d" add1res add2res
+    0 // return an integer exit code
