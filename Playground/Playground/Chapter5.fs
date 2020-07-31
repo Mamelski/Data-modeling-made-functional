88cc197a3decea0f939f8414821d9bb4f070ee82
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-07-31 20:15:53 +0200

    More fun with the types

--- Playground/Playground/Chapter5.fs
+++ Playground/Playground/Chapter5.fs
@@ -1,7 +1,18 @@
 ﻿module Playground.Chapter5
 
+type CustomerId = CustomerId of int
+type WidgetCode = WidgetCode of string
+type UnitQuantity = UnitQuantity of int
+type KilogramQuantity = KilogramQuantity of decimal
+
+let customerId = CustomerId 42
+
+let processCustomerId (CustomerId innerValue) =
+    printf "%A" innerValue
+
 [<EntryPoint>]
 let main argv =
 
+    processCustomerId customerId
     
     0 // return an integer exit code
