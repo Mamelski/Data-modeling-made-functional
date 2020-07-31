1f6a24faa364a685a4546450434d29d6441d9109
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-07-31 20:38:26 +0200

    Multiple params

--- Playground/Playground/Chapter5.fs
+++ Playground/Playground/Chapter5.fs
@@ -1,5 +1,7 @@
 ﻿module Playground.Chapter5
 
+open System.Net.NetworkInformation
+
 type CustomerId = CustomerId of int
 type WidgetCode = WidgetCode of string
 type UnitQuantity = UnitQuantity of int
@@ -10,6 +12,46 @@ let customerId = CustomerId 42
 let processCustomerId (CustomerId innerValue) =
     printf "%A" innerValue
 
+// nie referancyjny typ
+[<Struct>]
+type StructType = StructType of int
+
+// Jeśli jeszcze nie znamy szczegółów typów
+type Undefined = exn
+type CustomerInfo = Undefined
+type ShippingAddress = Undefined
+type BillingAddress = Undefined
+type OrderLine = Undefined
+type BillingAmount = Undefined
+type GizmoCode = Undefined
+
+type Order = {
+    CustomerInfo: CustomerInfo
+    ShippingAddress: ShippingAddress
+    BillingAddress: BillingAddress
+    OrderLine: OrderLine list
+    AmountToBill: BillingAmount
+}
+
+type ProductCode =
+    | Widget of WidgetCode
+    | Gizmo of GizmoCode
+    
+type OrderQuantity =
+    | UnitQuantity of UnitQuantity
+    | KilogramQuantity of KilogramQuantity
+    
+// funkcja z wieloma wyjściami
+type EnvelopeContents = EnvelopeContents of string
+type CategorizedEmail =
+    | Quote of string
+    | Order of string
+
+type CategorizeInboudEmail = EnvelopeContents -> CategorizedEmail
+
+// Funkcja z wieloma wejsciami (albo osobny typ na parametry)
+type CalculatePrices = Order -> ProductCode -> int
+ 
 [<EntryPoint>]
 let main argv =
 
