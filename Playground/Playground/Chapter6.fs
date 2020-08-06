71591aea81f6917dae38c98e0cfe7206fc20de52
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-08-06 19:57:15 +0200

    match value created with constructor

--- Playground/Playground/Chapter6.fs
+++ Playground/Playground/Chapter6.fs
@@ -1,6 +1,9 @@
 ﻿
 module Playground.Chapter6
 
+open Microsoft.VisualBasic
+open Playground.Chapter6.Module1
+
 module Module1 =
     type UnitQuantity = private UnitQuantity of int
 
@@ -17,9 +20,16 @@ module Module1 =
 module Module2 =
     //let value = UnitQuantity 1
     
+    let unitQty = Module1.UnitQuantity.create 1
+    
+    match unitQty with
+    | Error msg ->
+        printfn "Failure. Error is %A" msg
+    | Ok Qty ->
+        printfn "Ok, value is %A" Qty
+    
+    
 
 [<EntryPoint>]
 let main argv =
-    let u = UnitQuantity 10
-    printfn "%A" u
     0 // return an integer exit code
