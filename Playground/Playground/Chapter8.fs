034ac28c54f5127268705d5490ab5d780dc30294
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-08-07 21:44:50 +0200

    fun with list of functions

--- Playground/Playground/Chapter8.fs
+++ Playground/Playground/Chapter8.fs
@@ -1,13 +1,21 @@
 ﻿module Playground.Chapter8
 
 // Functionas are things
-let plus3 x = x +3
-let times2 x = x*2
-let square = (fun x -> x*x)
+let plus3 x = x + 3
+let times2 x = x * 2
+let square = (fun x -> x * x)
 let add3 = plus3
 
-
+let listOfFunctions =
+    [plus3;
+     times2;
+     square]
+    
 
 [<EntryPoint>]
 let main argv =
+    
+    for fn in listOfFunctions do
+        let result = fn 5
+        printfn "Result is %A" result
     0 // return an integer exit code
