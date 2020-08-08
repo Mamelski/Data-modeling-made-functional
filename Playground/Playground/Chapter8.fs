10f7a29f5eda376d224c81827c7e0cd7b1285831
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-08-08 15:03:57 +0200

    function as input parameter

--- Playground/Playground/Chapter8.fs
+++ Playground/Playground/Chapter8.fs
@@ -11,6 +11,8 @@ let listOfFunctions =
      times2;
      square]
     
+let evalWith6AndAdd1 fn =
+    fn(6) + 1
 
 [<EntryPoint>]
 let main argv =
@@ -18,4 +20,8 @@ let main argv =
     for fn in listOfFunctions do
         let result = fn 5
         printfn "Result is %A" result
+        
+    let res = evalWith6AndAdd1 square
+    printfn "Result is %A" res
+    
     0 // return an integer exit code
