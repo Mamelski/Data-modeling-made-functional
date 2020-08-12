b734a00924a6155f03696459f237371d62d2a7c7
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-08-12 22:12:09 +0200

    Use Active patterns

--- Playground/Playground/Chapter13.fs
+++ Playground/Playground/Chapter13.fs
@@ -1,5 +1,29 @@
 ﻿module Playground.Chapter13
 
+type Address = {
+    State : string
+    Country : string
+}
+type ValidatedOrder = {
+    ShippingAddress : Address
+    }
+
+let (|UsLocalState|UsRemoteState|International|) address = 
+    if address.Country = "US" then
+        match address.State with
+        | "CA" | "OR" | "AZ" | "NV" -> 
+            UsLocalState
+        | _ -> 
+            UsRemoteState
+    else
+        International
+            
+let calculateShippingCost validatedOrder = 
+    match validatedOrder.ShippingAddress with
+    | UsLocalState -> 5.0
+    | UsRemoteState -> 10.0
+    | International -> 20.0
+
 [<EntryPoint>]
 let main argv =
     
